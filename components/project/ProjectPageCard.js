76fce3c9466ffb88256399be7ad57987744d130c
Godwin Opara <godwinopara62@yahoo.com>
2023-05-16 12:44:48 +0100

    styled project page to be responsive on mobile and tablet

--- components/project/ProjectPageCard.js
+++ components/project/ProjectPageCard.js
@@ -1,17 +1,23 @@
 import Image from "next/image";
 import React from "react";
 import styles from "./Project.module.css";
+import Link from "next/link";
 
 const ProjectPageCard = ({ imgUrl, title, subtitle, year }) => {
 	return (
 		<div className={styles.projectImg__wrapper}>
 			<div className={styles.project__img}>
-				<Image src={imgUrl} height={400} width={600} alt="impression project image" loading="lazy" />
+				<Image src={imgUrl} fill={true} alt="impression project image" loading="lazy" />
 			</div>
-			<h3>{title}</h3>
+			<div className={styles.title}>
+				<h3>{title}</h3>
+				<span>{year}</span>
+			</div>
 			<div className={styles.subtitle}>
 				<p>{subtitle}</p>
-				<span>{year}</span>
+				<button>
+					<a href="https://github.com">View Live</a>
+				</button>
 			</div>
 		</div>
 	);
