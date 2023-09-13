95b4d2ea5b1900769273c9d014494c03062997e2
godwin opara <godwinopara62@yahoo.com>
2023-09-13 20:10:52 +0100

    added experience on about page

--- components/about/AboutMe.jsx
+++ components/about/AboutMe.jsx
@@ -19,6 +19,61 @@ const AboutMe = () => {
 					</div>
 				</div>
 			</div>
+
+			{/* Experience */}
+			<div className={S.experinceWrapper}>
+				<div className={S.experinceTitle}>
+					<h3>My Experiences</h3>
+					<p>
+						I have had the pleasure to work with companies across a variety of industries. I'm always interested in new, exciting and challenging adventures.
+					</p>
+					<button>Download Resume</button>
+				</div>
+				<div className={S.experinceContent}>
+					<div className={S.timeline}>
+						<div className={S.timelineInfo}>
+							<h4>NOV 2017 - PRESENT</h4>
+						</div>
+						<div className={S.timelineMarker}>
+							<div className={S.timelineDot}></div>
+						</div>
+						<div className={S.timelineContent}>
+							<h4>Creative Director at Malory House</h4>
+							<p>No salad. No salad. In front of the bow, you need to put the layer on the right side like a mauris. Until the hospital bed some as.</p>
+						</div>
+					</div>
+					<div className={S.timeline}>
+						<div className={S.timelineInfo}>
+							<h4>NOV 2017 - PRESENT</h4>
+						</div>
+						<div className={S.timelineMarker}>
+							<div className={S.timelineDot}></div>
+						</div>
+						<div className={S.timelineContent}>
+							<h4>Creative Director at Malory House</h4>
+							<p>No salad. No salad. In front of the bow, you need to put the layer on the right side like a mauris. Until the hospital bed some as.</p>
+						</div>
+					</div>
+					<div className={S.timeline}>
+						<div className={S.timelineInfo}>
+							<h4>NOV 2017 - PRESENT</h4>
+						</div>
+						<div className={S.timelineMarker}>
+							<div className={S.timelineDot}></div>
+						</div>
+						<div className={S.timelineContent} >
+							<h4>Creative Director at Malory House</h4>
+							<p>No salad. No salad. In front of the bow, you need to put the layer on the right side like a mauris. Until the hospital bed some as.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* Volunteering */}
+			<div></div>
+
+			{/* skills */}
+			<div></div>
 		</div>
 	);
 };
