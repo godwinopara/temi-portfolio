02e38e621350310441c6af97fa16397b443f8eeb
godwin opara <godwinopara62@yahoo.com>
2023-09-14 00:33:24 +0100

    styled about page

--- components/about/AboutMe.jsx
+++ components/about/AboutMe.jsx
@@ -4,34 +4,31 @@ import Experience from "../experiences/Experience";
 import Volunteer from "../volunteering/Volunteer";
 
 const AboutMe = () => {
-	return (
-		<div className="container">
-			<div className={S.aboutContainer}>
-				<div className={S.wrapper}>
-					<div className={S.imageWrapper}>
-						<Image src="/images/temi.jpg" fill alt="profile picture" />
-					</div>
-					<div className={S.descWrapper}>
-						<h2 className={S.descTitle}>Hello I'm Temitope Oladokun 😄</h2>
-						<p>
-							I'm Technical Product Manager from Nigeria.I Build unforgetable Solutions for Startups
-							and manage technical products that innovate with cutting-edge technology (Computer
-							Vision, NLP and Blockchain).
-						</p>
-					</div>
-				</div>
-			</div>
+  return (
+    <div className="container">
+      <div className={S.aboutContainer}>
+        <div className={S.wrapper}>
+          <div className={S.imageWrapper}>
+            <Image src="/images/temi.jpg" fill alt="profile picture" />
+          </div>
+          <div className={S.descWrapper}>
+            <h2 className={S.descTitle}>Hello I'm Temitope Oladokun 😄</h2>
+            <p>I'm Technical Product Manager from Nigeria.I Build unforgetable Solutions for Startups and manage technical products that innovate with cutting-edge technology (Computer Vision, NLP and Blockchain).</p>
+          </div>
+        </div>
 
-			{/* Experience */}
-			<Experience />
+        {/* Experience */}
+        <Experience />
 
-			{/* Volunteering */}
-			<Volunteer />
+        {/* Volunteering */}
+        <Volunteer />
 
-			{/* skills */}
-			<div></div>
-		</div>
-	);
+        {/* skills */}
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
+
--- components/timeline/Timeline.jsx
+++ components/timeline/Timeline.jsx
@@ -1,32 +1,25 @@
 import * as S from "./Timeline.module.css";
 
-const Timeline = ({
-	date,
-	position,
-	description,
-	description2 = "",
-	company,
-	location,
-	timelineMarkerClass,
-}) => {
-	return (
-		<div className={S.timeline}>
-			<div className={S.title}>
-				<h4>
-					{position} - <span>{company}</span>
-				</h4>
-			</div>
-			<div className={S.timelineInfo}>
-				<h4>
-					{date} | {location}
-				</h4>
-			</div>
-			<div className={S.timelineContent}>
-				<p>{description}</p>
-				<p>{description2}</p>
-			</div>
-		</div>
-	);
+const Timeline = ({ date, position, description, description2 = "", company, location, timelineMarkerClass }) => {
+  return (
+    <div className={S.timeline}>
+      <div className={S.timelineInfo}>
+        <h3>
+          {position} - <span>{company}</span>
+        </h3>
+        <p>
+          {date} | {location}
+        </p>
+      </div>
+      <div className={S.timelineContent}>
+        <ul className={S.list}>
+          <li>{description}</li>
+          {description2 && <li>{description2}</li>}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
+
